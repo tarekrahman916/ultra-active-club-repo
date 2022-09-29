91c3dce5286b3c4f58c427422b88cf6e6c20eaad
Md. Tarek Rahman <tarekrahman237@gmail.com>
2022-09-29 10:12:09 +0600

    add break data in Storage

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Summary.css";
 import profile from "../../images/profile.png";
 
 const Summary = ({ time }) => {
-  const breaksTime = [10, 20, 30, 40, 50];
+  const breakTimes = [10, 20, 30, 40, 50];
+  const [bTime, setBTime] = useState(0);
+
+  const handleBreakTime = (e) => {
+    // console.log(e.currentTarget.innerText);
+    const times = e.currentTarget.innerText;
+    localStorage.setItem("break", parseInt(times));
+    const storageBreaks = localStorage.getItem("break");
+    setBTime(parseInt(storageBreaks));
+  };
+
+  useEffect(() => {
+    const storageBreaks = localStorage.getItem("break");
+    setBTime(parseInt(storageBreaks));
+  }, []);
+
   return (
     <div className="summary border bg-white h-full p-5">
       <div className="info">
@@ -36,11 +51,15 @@ const Summary = ({ time }) => {
         <div className="mt-8">
           <h3 className="text-2xl font-semibold text-gray-700">Add a break</h3>
           <div className=" flex gap-4 justify-center mt-5 bg-gray-100">
-            <button className="break-btn btn">10m</button>
-            <button className="break-btn btn">20m</button>
-            <button className="break-btn btn">30m</button>
-            <button className="break-btn btn">40m</button>
-            <button className="break-btn btn">50m</button>
+            {breakTimes.map((breakTime) => (
+              <button
+                key={breakTime}
+                onClick={handleBreakTime}
+                className="break-btn btn"
+              >
+                {breakTime}m
+              </button>
+            ))}
           </div>
         </div>
         <div className="mt-7">
@@ -55,7 +74,7 @@ const Summary = ({ time }) => {
         <div className="mt-7">
           <div className="flex justify-around mt-5  bg-gray-200 py-5">
             <h2 className="text-lg font-semibold">Break time:</h2>
-            <h2>20 minutes</h2>
+            <h2>{bTime} minutes</h2>
           </div>
         </div>
         <button className="btn btn-success w-full mt-5 mb-10">Success</button>
